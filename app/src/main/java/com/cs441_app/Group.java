461de1b9f13b4d3b0104068ab86b55e9ccf13de2
Tyler Stratford <tyler.stratford1@gmail.com>
2019-05-05 00:13:23 -0700

    Login + more DH methods
    - added new createUser function to handle the firebase authentication
    - added setGroupSync method for saving a groups sync option for the user
    - added readGroups_BySync method that gets a list of groups the user has sycned to their calendar
    - added new parameters to some methods to allow for syncronization
    - added login feature
    - added some extra fields to Task and Group to support syncronization
    - added UserManager class that now handles block fetches for syncronization purposes
    - added  static void functions in MainActivity to support new login system

--- app/src/main/java/com/cs441_app/Group.java
+++ app/src/main/java/com/cs441_app/Group.java
@@ -3,18 +3,29 @@ package com.cs441_app;
 public class Group {
     private String groupID;
     private String name;
+    private boolean sync;
 
     public Group(){
-
+        sync = false;
     }
 
     public Group(String Name){
         name = Name;
+        sync = false;
     }
 
     public Group(String GroupID, String Name){
         groupID = GroupID;
         name = Name;
+        sync = false;
+    }
+
+    public boolean isSync() {
+        return sync;
+    }
+
+    public void setSync(boolean sync) {
+        this.sync = sync;
     }
 
     public String getGroupID() {
